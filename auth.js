c4ef21a39d007604c0ebb0b92a67589745cb43f8
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-01-24 19:35:06 +0100

    feat: Enhance authentication flow with password validation and user session management

--- auth.js
+++ auth.js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import User from '@/models/User';
+import bcrypt from 'bcrypt';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -17,8 +18,49 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           console.log('User not found');
           return null;
         }
-        
+        // check password if it's correct
+        const isPasswordValid = await bcrypt.compare(password, user?.password);
+        // if password is not correct
+        if (!isPasswordValid) {
+          console.log('Invalid password');
+          return null;
+        }
+
+        return {
+          id: user._id,
+          email: user.email,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          role: user.role,
+        };
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.firstName = user.firstName;
+        token.lastName = user.lastName;
+        token.role = user.role;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.email = token.email;
+        session.user.firstName = token.firstName;
+        session.user.lastName = token.lastName;
+        session.user.role = token.role;
+      }
+      return session;
+    },
+  },
+  session: {
+    strategy: 'jwt',
+    maxAge: 1000 * 60 * 60 * 24,
+  },
+  secret: process.env.AUTH_SECRET,
 });
--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import connectDB from '@/lib/db/connect';
-import User, { ROLES } from '@/models/User';
+import User from '@/models/User';
 import encryptPassword from '@/lib/encryptPassword';
 
 export async function POST(req) {

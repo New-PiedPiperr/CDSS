d554893f2b27a3d2f54695fb3d7de0d161d76674
SAMKIEL <samuelezekiel488@gmail.com>
2026-01-29 00:18:16 +0100

    feat: add initial authentication configuration with JWT and session callbacks.

---
+++ auth.config.js
@@ -0,0 +1,35 @@
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+    error: '/login',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.firstName = user.firstName;
+        token.lastName = user.lastName;
+        token.role = user.role;
+        token.avatar = user.avatar;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token && session.user) {
+        session.user.id = token.id;
+        session.user.email = token.email;
+        session.user.firstName = token.firstName;
+        session.user.lastName = token.lastName;
+        session.user.role = token.role;
+        session.user.avatar = token.avatar;
+      }
+      return session;
+    },
+  },
+  providers: [], // We will add providers in auth.js
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.AUTH_SECRET,
+};

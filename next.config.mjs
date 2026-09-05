35e9a76c29a46fc02ce875e3f9b532ec641b4e99
SAMKIEL <samuelezekiel488@gmail.com>
2026-09-05 12:56:45 +0100

    chore(config): update Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+
+ allowedDevOrigins: ['192.168.137.1'],
+
   reactCompiler: true,
   images: {
     remotePatterns: [
